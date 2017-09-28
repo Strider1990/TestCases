f780b882048546a5c645f7ccf3197dd87b8ef215
Alex Ooi <alexooi1990@gmail.com>
2017-09-28 14:57:33 +0800

    Removed useless comments

--- Archiver.cs
+++ Archiver.cs
@@ -14,34 +14,7 @@ public override void ItemUpdating(SPItemEventProperties properties)
 		Hashtable ht = properties.ListItem.Properties;
 		foreach (DictionaryEntry de in ht)
 			archiveItem[de.Key] = de.Value
-		/* In case Hashtable doesn't work
-		archiveItem["Title"] = properties.ListItem["Title"];
-		archiveItem["Request_Date"] = properties.ListItem["Request_Date"];
-		archiveItem["Network"] = properties.ListItem["Network"];
-		archiveItem["System"] = properties.ListItem["System"];
-		archiveItem["Task_To"] = properties.ListItem["Task_To"];
-		archiveItem["Project_Name"] = properties.ListItem["Project_Name"];
-		archiveItem["Request_Details"] = properties.ListItem["Request_Details"];
-		archiveItem["Business_Reason"] = properties.ListItem["Business_Reason"];
-		archiveItem["Expire_Date"] = properties.ListItem["Expire_Date"];
-		archiveItem["POC"] = properties.ListItem["POC"];
-		archiveItem["Status"] = properties.ListItem["Status"];
-		archiveItem["Tasker_Remarks"] = properties.ListItem["Tasker_Remarks"];
-		archiveItem["Message_To_Taskee"] = properties.ListItem["Message_To_Taskee"];
-		archiveItem["Taskee"] = properties.ListItem["Taskee"];
-		archiveItem["Message_To_Requestor"] = properties.ListItem["Message_To_Requestor"];
-		archiveItem["Approved_By"] = properties.ListItem["Approved_By"];
-		archiveItem["Tasked_By"] = properties.ListItem["Tasked_By"];
-		archiveItem["Taskee_Remarks"] = properties.ListItem["Taskee_Remarks"];
-		archiveItem["Deployment_Plan"] = properties.ListItem["Deployment_Plan"];
-		archiveItem["Requestor_Section"] = properties.ListItem["Requestor_Section"];
-		archiveItem["Implementation_Date"] = properties.ListItem["Implementation_Date"];
-		archiveItem["Is_Message_Read"] = properties.ListItem["Is_Message_Read"];
-		archiveItem["CNO"] = properties.ListItem["CNO"];
-		archiveItem["Start_Date"] = properties.ListItem["Start_Date"];
-		archiveItem["Rejected_Remarks"] = properties.ListItem["Rejected_Remarks"];
 		archiveItem.Update();
-		*/
 
 		/* Delete relevant list item from list_request */
 		SPListItemCollection listItems = properties.List.Items;
